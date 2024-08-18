2960d53843748016ecfddc239a1397b0998fb6c8
Thilan Dissanayaka <43875064+Thil4n@users.noreply.github.com>
2024-08-18 17:25:15 +0530

    init

---
+++ buuble-sort.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+using namespace std;
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void bubble_sort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(&arr[j], &arr[j + 1]);
+            }
+        }
+    }
+}
+
+void print_array(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main(int argc, char const *argv[])
+{
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    bubble_sort(arr, n);
+    print_array(arr, n);
+    return 0;
+}
