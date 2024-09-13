41e146db3cfec4d983c92113cd9dfe2b34c87036
Thilan Dissanayaka <43875064+Thil4n@users.noreply.github.com>
2024-09-13 16:10:19 +0530

    update

--- min_coin.cpp
+++ min_coin.cpp
@@ -7,7 +7,7 @@ int coins[] = {1, 4, 5};
 
 int num_coins = sizeof(coins) / sizeof(coins[0]);
 
-int minimum_coins(int m, int coin_count, vector<int> &memo)
+int minimum_coins(int m, vector<int> &memo)
 {
 
     if (memo[m] != -1)
@@ -31,7 +31,7 @@ int minimum_coins(int m, int coin_count, vector<int> &memo)
             continue;
         }
 
-        int x = minimum_coins(m - coin, coin_count + 1, memo);
+        int x = minimum_coins(m - coin, memo);
         if (x < min)
         {
             min = x;
@@ -49,6 +49,6 @@ int main(int argc, char const *argv[])
 
     vector<int> memo(amount + 1, -1);
 
-    cout << minimum_coins(amount, 0, memo) << endl;
+    cout << minimum_coins(amount, memo) << endl;
     return 0;
 }
